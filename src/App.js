5b09779e588bc6df0f093fe19171313d736e11b4
Dan Pisarev <danila.pisarev@facecast.net>
2023-12-04 11:43:26 +0300

    Add codeowner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import * as Sentry from "@sentry/react";
+
+const release = "sentry-react@1.0.0";
+
+class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = `ERROR: ${message} from ${release}`;
+  }
+}
+
+Sentry.init({
+  dsn: "https://b2324bc012cc51f4f05ca25d5bbe2cf7@o4506313834692608.ingest.sentry.io/4506336320880640",
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+
+  release,
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 function App() {
+  const handleClick = (message) => {
+    throw new ValidationError(message);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +39,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={() => {
+            handleClick("Houston we have a 2nd error");
+          }}
         >
           Learn React
-        </a>
+        </button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default Sentry.withProfiler(App);
